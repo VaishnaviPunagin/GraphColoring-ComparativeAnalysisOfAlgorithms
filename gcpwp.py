48a3e5fa38c24158aeee538f7494993cd88a8a7a
anupam-ahi <56450488+anupam-ahi@users.noreply.github.com>
2020-05-23 16:45:16 +0530

    Update gcpwp.py

--- gcpwp.py
+++ gcpwp.py
@@ -1,5 +1,33 @@
 import time
-from gcpgy import neighbors, colors
+from input_values import neighbors, colors
+
+start = time.time()
+# n = int(input("Enter number of colours : "))
+# colors = []
+# for i in range(0, n):
+#     print("Enter the %d color:" % (i+1))
+#     ele = input()
+#     colors.append(ele)
+# print(colors)
+# x = int(input("Enter number of nodes : "))
+# nodes = []
+# for i in range(0, x):
+#     print("Enter the %d node " % (i+1))
+#     ele = int(input())
+#     nodes.append(ele)
+# print(nodes)
+# neighbors = {}
+# for i in range(0, x):
+#     key = nodes[i]
+#     print("Enter the number of connections for %d node and its connections :" % key)
+#     z = int(input())
+#     my_list = []
+#     for state in range(0, z):
+#         ele = int(input())
+#         my_list.append(ele)
+#     neighbors.update({key: my_list})
+# colors_of_states = {}
+
 graph = neighbors
 color_map = colors
 
@@ -14,21 +42,12 @@ def color_nodes(graph):
         )
     return color_map
 
-#starting time
-start =  time.time()
+
 if __name__ == '__main__':
 
     print(color_nodes(graph))
     # {'c': 0, 'a': 1, 'd': 2, 'e': 1, 'b': 2, 'f': 2}
-#sleeping for 1 second to get 10 seconds runtime
 time.sleep(1)
-
-# program body ends
-
-# end time
 end = time.time()
-
-tt=end-start 
-# total time taken
-print("Runtime of the program is")
-print(tt)
+tt = end-start
+print("The time taken for welsh powell algorithm is %d" % tt)
