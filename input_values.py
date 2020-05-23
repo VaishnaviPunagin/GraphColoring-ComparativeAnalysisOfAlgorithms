ec480acb7f754302e645aad58d8dd2736e6e12c1
anupam-ahi <56450488+anupam-ahi@users.noreply.github.com>
2020-05-23 16:46:20 +0530

    Create input_values.py

---
+++ input_values.py
@@ -0,0 +1,25 @@
+
+n = int(input("Enter number of colours : "))
+colors = []
+for i in range(0, n):
+    print("Enter the %d color:" % (i+1))
+    ele = input()
+    colors.append(ele)
+print(colors)
+x = int(input("Enter number of nodes : "))
+nodes = []
+for i in range(0, x):
+    print("Enter the %d node " % (i+1))
+    ele = int(input())
+    nodes.append(ele)
+print(nodes)
+neighbors = {}
+for i in range(0, x):
+    key = nodes[i]
+    print("Enter the number of connections for {%d}:" % key)
+    z = int(input())
+    my_list = []
+    for state in range(0, z):
+        ele = int(input())
+        my_list.append(ele)
+    neighbors.update({key: my_list})
