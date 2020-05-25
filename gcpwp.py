acb10118da4ee0c6c6fbd90a4e918c59cf2428be
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2020-05-26 04:06:11 +0530

    Update gcpwp.py

--- gcpwp.py
+++ gcpwp.py
@@ -1,53 +1,29 @@
-import time
-from input_values import neighbors, colors
+import matplotlib.pyplot as plt
+print("------------------------------------------")
+print("Analysis section of the graph coloring problem")
+print("We use 3 different algorithms!")
+print("Now, come, give us the data!")
+print("------------------------------------------")
+from gcpgy import tt
+gtt=tt
 
-start = time.time()
-# n = int(input("Enter number of colours : "))
-# colors = []
-# for i in range(0, n):
-#     print("Enter the %d color:" % (i+1))
-#     ele = input()
-#     colors.append(ele)
-# print(colors)
-# x = int(input("Enter number of nodes : "))
-# nodes = []
-# for i in range(0, x):
-#     print("Enter the %d node " % (i+1))
-#     ele = int(input())
-#     nodes.append(ele)
-# print(nodes)
-# neighbors = {}
-# for i in range(0, x):
-#     key = nodes[i]
-#     print("Enter the number of connections for %d node and its connections :" % key)
-#     z = int(input())
-#     my_list = []
-#     for state in range(0, z):
-#         ele = int(input())
-#         my_list.append(ele)
-#     neighbors.update({key: my_list})
-# colors_of_states = {}
+from gcpwp import tt
+wtt=tt
 
-graph = neighbors
-color_map = colors
+from gcpbt import tt
+btt=tt
 
+import numpy as np
 
-def color_nodes(graph):
-    color_map = {}
-    # Consider nodes in descending degree
-    for node in sorted(graph, key=lambda x: len(graph[x]), reverse=True):
-        neighbor_colors = set(color_map.get(neigh) for neigh in graph[node])
-        color_map[node] = next(
-            color for color in range(len(graph)) if color not in neighbor_colors
-        )
-    return color_map
-
-
-if __name__ == '__main__':
-
-    print(color_nodes(graph))
-    # {'c': 0, 'a': 1, 'd': 2, 'e': 1, 'b': 2, 'f': 2}
-time.sleep(1)
-end = time.time()
-tt = end-start
-print("The time taken for welsh powell algorithm is %d" % tt)
+alg=['greedy', 'backtracking','welshpowell']
+tt=[gtt,btt,wtt]
+ypos=np.arange(len(alg))
+plt.xticks(ypos,alg)
+plt.ylabel("Time(s)")
+plt.xlabel("greedy vs backtracking vs welsh powell")
+plt.title ("Effeciencies Analysis of greedy vs backtracking vs welshpowell algorithms")
+l=plt.bar(ypos,tt)
+l[0].set_color('r')
+l[1].set_color('g')
+plt.legend()
+plt.show()
